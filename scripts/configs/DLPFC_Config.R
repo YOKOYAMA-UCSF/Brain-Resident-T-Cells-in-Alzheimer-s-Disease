cc352f563011c3bac4b844885763c1cff86ef833
Jake Oxendine <jake.oxendine@ucsf.edu>
2025-08-12 18:49:43 +0000

    name change of PFC config to DLPFC config

--- scripts/configs/DLPFC_Config.R
+++ scripts/configs/DLPFC_Config.R
@@ -1,6 +1,3 @@
 # Config file designed for replicating analysis for a specific brain region
 
 PATH_TO_SEURAT_OBJ="path/to/SEA-AD/145aadbe-fd91-4592-bf14-3b4c644a2141"
-
-# Clusters indenfied as Lymphocytes during initial analysis workflow (Filled in)
-LYMPHOCYTE_CLUSTER_LIST=c(22)
